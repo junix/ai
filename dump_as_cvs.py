cde860201af0b7b7262529344a0e71b2f8e62831
wanglijun <juns1984@qq.com>
2017-03-31 14:07:39 +0800

    init

---
+++ dump_as_cvs.py
@@ -0,0 +1,26 @@
+import jieba.posseg
+import os
+import pandas
+import pandas as pd
+
+
+def clean(text):
+    text = text.replace(',', '')
+    text = text.replace('\n', '')
+    return text
+
+
+if __name__ == '__main__':
+    corpus_dir = '/Users/junix/products'
+    columns = ['category', 'description']
+    df = pd.DataFrame(data=[], index=[], columns=columns)
+    for root, _, files in os.walk(corpus_dir):
+        category = os.path.basename(root)
+        for product in files:
+            with open(root + '/' + product, 'r') as fd:
+                content = ''.join(fd.readlines())
+                record = [clean(category), clean(content)]
+                rec = pd.DataFrame(data=[record], index=[clean(product)], columns=columns)
+                df = df.append(rec)
+
+    df.to_csv('/Users/junix/products_pos.csv')
